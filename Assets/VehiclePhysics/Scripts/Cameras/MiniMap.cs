2a8668ab2dc34423b4176a7a6f38cb33e62bdb1a
wangcong <1043947508@qq.com>
2024-05-13 14:56:13 +0800

    分次7

---
+++ Assets/VehiclePhysics/Scripts/Cameras/MiniMap.cs
@@ -0,0 +1,175 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 小地图
+/// </summary>
+public class MiniMap : MonoBehaviour
+{
+    //private Transform miniCarIcon;//车辆图标
+    private Transform miniPlayerIcon;//人物图标
+    private Text jingWeiText;
+    private float mapSize;
+
+    /// <summary>
+    /// 地图缩放 最小值
+    /// </summary>
+    private const float MIN_SIZE = 50;
+
+    /// <summary>
+    /// 地图缩放最大值
+    /// </summary>
+    private const float MAX_SIZE = 2500;
+
+    /// <summary>
+    /// 大地图按钮
+    /// </summary>
+    private ButtonBase maxMapBtn;
+
+    /// <summary>
+    /// 车角度
+    /// </summary>
+    private Vector3 carAngle;
+    // Start is called before the first frame update
+
+    /// <summary>
+    /// 小地图渲ui
+    /// </summary>
+    private RawImage rawImage;
+
+    /// <summary>
+    /// 小地图宽高最低长度
+    /// </summary>
+    private const int MIN_MAP_SIZE = 300;
+
+    private ButtonBase plusBtn;
+
+    private ButtonBase subBtn;
+
+    /// <summary>
+    /// 小地图缩放 变动值
+    /// </summary>
+    private const float MAP_SCALE_CHANGE = 25;
+    private void Awake()
+    {
+        maxMapBtn = transform.Find("maxMapBtn").GetComponent<ButtonBase>();
+        //miniCarIcon = transform.Find("CarIcon");
+        miniPlayerIcon = transform.Find("PlayIcon");
+        jingWeiText = transform.Find("JingWeiText").GetComponent<Text>();
+        rawImage = transform.Find("mask/rawImage").GetComponent<RawImage>();
+        plusBtn = transform.Find("plusBtn").GetComponent<ButtonBase>();
+        plusBtn.RegistClick(OnClickPlusBtn);
+        subBtn = transform.Find("subBtn").GetComponent<ButtonBase>();
+        mapSize = 100;
+        subBtn.RegistClick(OnClickSubBtn);
+    }
+
+    private void Start()
+    {
+        InitMinimapSize();
+    }
+
+    private void UpdateCarText()
+    {
+        if (SceneMgr.GetInstance().curScene is Train3DSceneCtrBase scene3D)
+        {
+            List<Vector3> tempList = Line3dControl.Instance.GetListPoint();
+            if (tempList.Count == 0) return;
+            Vector3 car = scene3D.miniMapMgr.MiniMapCamera.GetPoint();
+            if (MathTool.IsPointInPolygon(car, tempList))
+            {
+                transform.Find("WarningText").gameObject.SetActive(false);
+            }
+            else
+            {
+                transform.Find("WarningText").gameObject.SetActive(true);
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //更新经纬度
+        UpdateLation();
+        UpdateCarText();
+    }
+
+    /// <summary>
+    /// 更新经纬度
+    /// </summary>
+    private void UpdateLation()
+    {
+        if (SceneMgr.GetInstance().curScene is Train3DSceneCtrBase scene3D)
+        {
+            carAngle = scene3D.miniMapMgr.MiniMapCamera.GetAngle();
+            Vector3 lation = scene3D.terrainChangeMgr.gisPointMgr.GetGisPos(scene3D.miniMapMgr.MiniMapCamera.GetPoint());
+            if (lation != null)
+                jingWeiText.text = "经度：" + lation.x + "，纬度：" + lation.y;
+            //if (GameObject.Find("RigidBodyFPSController") == true)
+            //{
+            //    miniPlayerIcon.eulerAngles = new Vector3(0, 0, -carAngle.y);
+            //    miniCarIcon.gameObject.SetActive(false);
+            //    miniPlayerIcon.gameObject.SetActive(true);
+            //}
+            //else
+            {
+/*                miniCarIcon.eulerAngles = new Vector3(0, 0, -carAngle.y);
+                miniCarIcon.gameObject.SetActive(true);*/
+                miniPlayerIcon.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 初始化小地图尺寸
+    /// </summary>
+    private void InitMinimapSize()
+    {
+        //屏幕宽高
+        int screenWidth = Screen.width;
+        int screenHeight = Screen.height;
+        int widthRatio = screenWidth / MIN_MAP_SIZE;
+        int heightRatio = screenHeight / MIN_MAP_SIZE;
+        int minRatio = Mathf.Min(widthRatio, heightRatio);
+        //根据屏幕宽高 算出ui 保证不小于MIN_MAP_SIZE的最小宽高
+        float resHeight = screenHeight * 1.0f / minRatio;
+        float resWidth = screenWidth * 1.0f / minRatio;
+        //修改ui大小
+        rawImage.rectTransform.sizeDelta = new Vector2(resWidth, resHeight);
+        //修改renderTexture分辨率 后续如果小地图精度过低，可适当在这里增加renderTexture分辨率
+        if (rawImage.texture != null && rawImage.texture is RenderTexture renderTexture)
+        {
+            renderTexture.width = (int)resWidth;
+            renderTexture.height = (int)resHeight;
+        }
+    }
+
+    private void OnClickPlusBtn(GameObject obj)
+    {
+        ChangeMapSize(-MAP_SCALE_CHANGE);
+    }
+
+    private void OnClickSubBtn(GameObject obj)
+    {
+        ChangeMapSize(MAP_SCALE_CHANGE);
+    }
+
+    //放大缩小
+    public void ChangeMapSize(float value)
+    {
+        mapSize += value;
+        mapSize = Mathf.Clamp(mapSize, MIN_SIZE, MAX_SIZE);
+        if (SceneMgr.GetInstance().curScene is Train3DSceneCtrBase scene3D)
+        {
+            scene3D.miniMapMgr.MiniMapCamera.SetOrthSize(mapSize);
+
+        }
+    }
+
+
+
+}
---
+++ Assets/VehiclePhysics/Scripts/Point/CarGisPointMgr.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarGisPointMgr 
+{    
+
+    /// <summary>
+    /// 左上角经纬度
+    /// </summary>
+    private Vector2 topLeftGis;
+
+    /// <summary>
+    /// 右下角经纬度
+    /// </summary>
+    private Vector2 bottomRightGis;
+
+    /// <summary>
+    /// 左上角地图坐标
+    /// </summary>
+    private Vector3 topLeftPos;
+
+    /// <summary>
+    /// 右下角地图坐标
+    /// </summary>
+    private Vector3 bottomRightPos;
+
+    /// <summary>
+    /// 三维地图与二维地图的比值
+    /// </summary>
+    private Vector2 radio;
+
+
+    /// <summary>
+    /// 经纬度转unity坐标
+    /// </summary>
+    public Vector3 GetTerrainPos(Vector2 gisPos)
+    {
+        GetGisAndTerrainRadio();
+        Vector3 tempVector3;
+        tempVector3 = new Vector3((gisPos.x - topLeftGis.x) * radio.x, 50, topLeftPos.z + ((gisPos.y - topLeftGis.y) * radio.y));
+        return tempVector3;
+    }
+
+
+    /// <summary>
+    /// unity坐标转经纬度坐标
+    /// </summary>
+    public Vector2 GetGisPos(Vector3 terrainPos)
+    {
+        GetGisAndTerrainRadio();
+        Vector2 tempVector2;
+        tempVector2 = new Vector2(topLeftGis.x+(terrainPos.x - topLeftPos.x) / radio.x, topLeftGis.y + (terrainPos.z - topLeftPos.z) / radio.y);
+        return tempVector2;
+    }
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    public CarGisPointMgr(Vector2 startGis, Vector2 endGis, Vector3 terrainSize)
+    {
+        topLeftGis = startGis;
+        bottomRightGis = endGis;
+        topLeftPos = new Vector3(0, 0, terrainSize.z);
+        bottomRightPos = new Vector3(terrainSize.x, 0, 0);
+    }
+
+    /// <summary>
+    /// 获取二维地图和三维地图的比值
+    /// </summary>
+    private void GetGisAndTerrainRadio()
+    {
+        float gisxDistance = bottomRightGis.x - topLeftGis.x;
+        float gisyDistance = bottomRightGis.y - topLeftGis.y;
+        float terrainxDistance = bottomRightPos.x - topLeftPos.x;
+        float terrainyDistance = bottomRightPos.z - topLeftPos.z;
+        radio = new Vector2(terrainxDistance/gisxDistance, terrainyDistance/gisyDistance);
+
+
+
+    }
+
+}
